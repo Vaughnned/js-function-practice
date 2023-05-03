bb9fd5e1d01138aa00f516201cc7188d31b0e5cf
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-03 09:02:27 -0400

    typo

--- js/exercises.js
+++ js/exercises.js
@@ -92,10 +92,7 @@
   // ---------------------
   // Define a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
   function filterLongWords(longWords) {
-    longWords = ["big", "bigger", "biggest"] {
-      
-    }
-
+    longWords = ["big", "bigger", "biggest"];
   }
   // ---------------------
 
