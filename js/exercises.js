66fdb7a2ed52e98ac9719c20ceb7b2192a97819c
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-03 10:28:16 -0400

    Reviewing code and adding notes

--- js/exercises.js
+++ js/exercises.js
@@ -25,12 +25,15 @@
     if (num1 > num2 && num1 > num3) {
       return num1;
     } else if (num2 > num1 && num2 > num3) {
+      // ignore num 1 after first if statement
       return num2;
     } else {
       return num3;
     }
   }
   console.log(maxOfThree(num1, num2, num3));
+  // maxOfThree(4, 8, 2); would be cleaner;
+
   // ---------------------
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
@@ -38,7 +41,8 @@
   let array = ["a", "e", "i", "o", "u"];
   function isVowel(character) {
     if (array.includes(character)) {
-      return true;
+      // return [ array ].includes(char);
+      return true; // return 'aeiou'.includes(char); these are better options
     } else {
       return false;
     }
@@ -47,13 +51,26 @@
   // ---------------------
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
-
+  function rovarspraket(text) {
+    let translation = "";
+    const textArr = text.split(""); //['h', 'a', 'p', 'p', 'y']
+    for (let i = 0; i < textArr.length; i++) {
+      const isConsonant = "bcdfghjklmnpqrstvwxyz".includes(textArr[i]);
+      if (isConsonant) {
+        translation = translation + textArr[i] + "o" + textArr[i];
+      } else {
+        translation = translation + textArr[i];
+      }
+    }
+    return translation;
+  }
+  rovarspraket("happy"); // "hohapoppopyoy"
   // ---------------------
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
   // ---------------------
 
-  let arrayMultiplied = 1;
-  let arraySum = 0;
+  const arrayMultiplied = 1;
+  const arraySum = 0;
   let numberArray = [2, 4, 6, 3];
   function sum(arraySum) {
     for (i = 0; i < numberArray.length; i++) {
@@ -80,7 +97,16 @@
     joinArray = reversedArray.join("");
     return joinArray;
   }
+
+  //return str.splt("").reverse().join(""); Much cleaner way to reverse
   reverse("jag testar");
+
+  // Another way of reversing
+
+  // for(let i = str.length - 1; i >= 0; i--;) {
+  //  reversedArray += str[i];
+  // }
+  // return reversedArray;
 
   // ---------------------
   // Define a function findLongestWord() that takes an array of words and returns the length of the longest one.
@@ -89,10 +115,26 @@
     words = [["long"], ["longer"], ["longest"]];
   }
   console.log(words[2][0].length);
+
+  // For loop is better. Prevents hard coding
+  // let length = 0;
+  // for (let i = 0; i < arr.length; i++) {
+  //  if(arr[i].length < length) {
+  //    length = arr[i].length;
+  //   }
+  // return length
+  // }
+
   // ---------------------
   // Define a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
-  function filterLongWords(longWords) {
-    longWords = ["big", "bigger", "biggest"];
+  function filterLongWords(arr, i) {
+    const filteredWordsArr = [];
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j].length > i) {
+        filteredWordsArr.push(arr[j]);
+      }
+    }
+    return filteredWordsArr;
   }
   // ---------------------
 
@@ -115,7 +157,20 @@
   // ---------------------
   // Define a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
   // ---------------------
-
+  function charFreq(str) {
+    // str = books
+    const freqList = [];
+    const strArr = str.split(""); // ['b', 'o', 'o', 'k', 's']
+    for (let i = 0; i < strArr.length; i++) {
+      const char = strArr[i]; // simplifies code
+      if (freqList[char] !== undefined) {
+        freqList[char] += 1;
+      } else {
+        freqList[char] = 1;
+      }
+      // freqList[char] += 1 || 1; Cleaner for loop
+    }
+  }
   ////////////////////////////////////////////////////////////////////////
   /////////////////////////DO NOT CHANGE CODE BELOW///////////////////////
   ////////////////////////////////////////////////////////////////////////
